62ef53c4a80788cda90b5645917cb07c86a2ea5a
MyoMyatAung <myatmyo299@gmail.com>
2023-04-18 15:37:39 +0630

    update webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,9 +2,12 @@ const express = require("express");
 let router = express.Router();
 
 const { getHomePage } = require("../controllers/homepageController");
+const { postWebhook, getWebhook } = require("../controllers/chatbotController");
 
 let initWebRoutes = (app) => {
   router.get("/", getHomePage);
+  router.post("/webhook", postWebhook);
+  router.get("/webhook", getWebhook);
 
   return app.use("/", router);
 }
