73b5af0936b16b50975aae4bee1f6a289cedeb23
MyoMyatAung <myatmyo299@gmail.com>
2023-04-20 05:03:05 +0630

    Update

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,7 +1,7 @@
 const express = require("express");
 let router = express.Router();
 
-const { getHomePage } = require("../controllers/homepageController");
+const { getHomePage, getFacebookUserProfile, setUpUserFacebookProfile } = require("../controllers/homepageController");
 const { postWebhook, getWebhook } = require("../controllers/chatbotController");
 
 let initWebRoutes = (app) => {
@@ -9,6 +9,8 @@ let initWebRoutes = (app) => {
   router.get("/greeting", getHomePage);
   router.post("/webhook", postWebhook);
   router.get("/webhook", getWebhook);
+  router.get("/profile", getFacebookUserProfile);
+  router.post("/set-up-user-fb-profile", setUpUserFacebookProfile)
 
   return app.use("/", router);
 }
